09d69e6dbe7b0ec814211ee9ff6fed0a8a317886
NgollrRomaric <ngolleromaric27@gmail.com>
2025-02-04 19:36:06 +0100

    Finalyse projet

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import IconPicker from "./components/IconPicker";
 import { useState } from "react";
 import { Download, icons } from "lucide-react";
 import ColorPicker from "./components/ColorPicker";
+import React from "react";
 
 type IconName = keyof typeof icons;
 
@@ -73,6 +74,154 @@ export default function Home() {
       ? { background: backgroundColor }
       : { backgroundColor: backgroundColor };
   };
+
+  const getPresetBackgroundStyle = (color: string) => {
+    return color.startsWith("linear-gradient")
+      ? { background: color }
+      : { backgroundColor: color };
+  };
+
+  const logoPresets = [
+    {
+      id: 1,
+      backgroundColor:
+        "linear-gradient(45deg, rgba(255, 126, 95, 1) 0%, rgba(254, 180, 123, 1) 100%)", // Dégradé chaleureux
+      radius: 8,
+      fillColor: "blue",
+      iconRotation: 0,
+      iconStrokeColor: "white",
+      iconStrokeWidth: 2,
+      iconSize: 200,
+      icon: "VenetianMask",
+    },
+    {
+      id: 2,
+      backgroundColor:
+        "linear-gradient(45deg, rgba(255, 111, 97, 1) 0%, rgba(255, 185, 120, 1) 100%)", // Dégradé orange doux
+      radius: 12,
+      fillColor: "yellow",
+      iconRotation: 15,
+      iconStrokeColor: "black",
+      iconStrokeWidth: 3,
+      iconSize: 220,
+      icon: "Apple",
+    },
+    {
+      id: 3,
+      backgroundColor:
+        "linear-gradient(45deg, rgba(79, 172, 254, 1) 0%, rgba(0, 242, 254, 1) 100%)", // Dégradé bleu clair
+      radius: 16,
+      fillColor: "violet",
+      iconRotation: 30,
+      iconStrokeColor: "white",
+      iconStrokeWidth: 4,
+      iconSize: 240,
+      icon: "Bell",
+    },
+    {
+      id: 4,
+      backgroundColor:
+        "linear-gradient(45deg, rgba(159, 86, 217, 1) 0%, rgba(52, 152, 219, 1) 100%)", // Dégradé violet/bleu
+      radius: 28,
+      fillColor: "black",
+      iconRotation: 90,
+      iconStrokeColor: "orange",
+      iconStrokeWidth: 4,
+      iconSize: 300,
+      icon: "Heart",
+    },
+    {
+      id: 5,
+      backgroundColor:
+        "linear-gradient(45deg, rgba(255, 175, 189, 1) 0%, rgba(255, 195, 160, 1) 100%)", // Dégradé rose/jaune
+      radius: 32,
+      fillColor: "white",
+      iconRotation: 120,
+      iconStrokeColor: "#f1c40f",
+      iconStrokeWidth: 2,
+      iconSize: 220,
+      icon: "MessageCircle",
+    },
+    {
+      id: 6,
+      backgroundColor: "#F29F58", // Dégradé magenta/orange
+      radius: 36,
+      fillColor: "#800080",
+      iconRotation: 150,
+      iconStrokeColor: "#fff",
+      iconStrokeWidth: 3,
+      iconSize: 240,
+      icon: "Sun",
+    },
+    {
+      id: 7,
+      backgroundColor:
+        "linear-gradient(45deg, rgba(54, 209, 220, 1) 0%, rgba(91, 134, 229, 1) 100%)", // Dégradé bleu/vert
+      radius: 40,
+      fillColor: "#008000",
+      iconRotation: 180,
+      iconStrokeColor: "white",
+      iconStrokeWidth: 4,
+      iconSize: 260,
+      icon: "Zap",
+    },
+    {
+      id: 8,
+      backgroundColor: "red", // Dégradé bleu/rouge
+      radius: 14,
+      fillColor: "blue",
+      iconRotation: 75,
+      iconStrokeColor: "#fff",
+      iconStrokeWidth: 3,
+      iconSize: 200,
+      icon: "Smile",
+    },
+    {
+      id: 9,
+      backgroundColor: "#4335A7", // Dégradé vert/rose
+      radius: 18,
+      fillColor: "red",
+      iconRotation: 135,
+      iconStrokeColor: "red",
+      iconStrokeWidth: 4,
+      iconSize: 280,
+      icon: "ArrowUp",
+    },
+    {
+      id: 10,
+      backgroundColor:
+        "linear-gradient(45deg, rgba(255, 105, 180, 1) 0%, rgba(244, 67, 54, 1) 100%)", // Dégradé rose/rouge
+      radius: 22,
+      fillColor: "#f1c40f",
+      iconRotation: 75,
+      iconStrokeColor: "#000",
+      iconStrokeWidth: 2,
+      iconSize: 210,
+      icon: "Calendar",
+    },
+    {
+      id: 11,
+      backgroundColor: "#D3F1DF", // Dégradé vert/orange
+      radius: 20,
+      fillColor: "#e84393",
+      iconRotation: 165,
+      iconStrokeColor: "#FFF",
+      iconStrokeWidth: 3,
+      iconSize: 300,
+      icon: "Star",
+    },
+    {
+      id: 12,
+      backgroundColor: "#525B44", // Dégradé orange/bleu
+      radius: 26,
+      fillColor: "#8e44ad",
+      iconRotation: 180,
+      iconStrokeColor: "white",
+      iconStrokeWidth: 4,
+      iconSize: 250,
+      icon: "Cloud",
+    },
+  ];
 
   return (
     <div>
@@ -259,6 +408,39 @@ export default function Home() {
               onChange={handleRadiusChange}
               className="range"
             />
+          </div>
+
+          <div className="mt-5">
+            <h3 className="text-lg font-bold mb-4">Préréglage</h3>
+            <div className="flex flex-wrap gap-2">
+              {logoPresets.map((preset) => (
+                <div key={preset.id} className="pointer">
+                  <div
+                    id="iconContainer"
+                    className={`w-16 h-16 flex justify-center items-center ${Shadow}`}
+                    style={{
+                      ...getPresetBackgroundStyle(preset.backgroundColor),
+                      borderRadius: `${preset.radius}px`,
+                    }}
+                  >
+                    {icons[preset.icon as keyof typeof icons] &&
+                      React.createElement(
+                        icons[preset.icon as keyof typeof icons],
+                        {
+                          size: 30,
+                          style: {
+                            strokeWidth: IconStrokeWidth,
+                            fill: preset.fillColor,
+                            stroke: preset.iconStrokeColor,
+                            display: "block",
+                            transform: `rotate(${IconRotation}deg)`,
+                          },
+                        }
+                      )}
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </section>
